885a07a545a9468edf432a5576ceb8ed4c11b1cb
Valtteri Puhakka <puhakka.valtteri@gmail.com>
2017-11-21 14:55:12 +0200

    julkaisukelpoiseksi muutettu fetchit

--- src/main/java/com/haagahelia/okt/Flights/FlightsApplication.java
+++ src/main/java/com/haagahelia/okt/Flights/FlightsApplication.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import com.haagahelia.okt.Flights.domain.Flight;
@@ -12,10 +14,14 @@ import com.haagahelia.okt.Flights.domain.FlightRepository;
 
 
 @SpringBootApplication
-public class FlightsApplication {
+public class FlightsApplication extends SpringBootServletInitializer{
 	
 	private static final Logger log = LoggerFactory.getLogger(FlightsApplication.class);
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FlightsApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FlightsApplication.class, args);
 	}
